c61874cd71b8784f702fd42ad1266a00bde6d8cb
joluhan <135083872+joluhan@users.noreply.github.com>
2024-01-04 13:52:00 +0100

    Update views.py

--- review/ltreview_app/views.py
+++ review/ltreview_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 # from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
-from forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm
 
 class SignUpView(generic.CreateView):
     form_class = CustomUserCreationForm
