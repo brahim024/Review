4031a1925686da32490ed270b20abfe84f07b742
joluhan <135083872+joluhan@users.noreply.github.com>
2024-01-04 10:38:35 +0100

    Summary

--- review/review/urls.py
+++ review/review/urls.py
@@ -16,6 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from ltreview_app.views import SignUpView
+from django.contrib.auth.views import LoginView, LogoutView
+
+urlpatterns = [
+    path('signup/', SignUpView.as_view(), name='signup'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+]
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
