7f0b5c7d5bc5b27b549aaa2aa7dc4558231f5acc
joluhan <135083872+joluhan@users.noreply.github.com>
2024-01-04 09:38:28 +0100

    Summary

--- review/ltreview_app/models.py
+++ review/ltreview_app/models.py
@@ -1,6 +1,8 @@
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.conf import settings
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class Ticket(models.Model):
@@ -27,3 +29,10 @@ class UserFollows(models.Model):
         # ensures we don't get multiple UserFollows instances
         # for unique user-user_followed pairs
         unique_together = ('user', 'followed_user', )
+
+class CustomUser(AbstractUser):
+    # Champ supplémentaire : livre préféré de l'utilisateur
+    # favorite_book = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return self.username
