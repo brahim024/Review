dbca493c89c5b12cae8f856be50f7e3fff69bef7
joluhan <135083872+joluhan@users.noreply.github.com>
2024-01-04 11:07:46 +0100

    summary

--- review/ltreview_app/models.py
+++ review/ltreview_app/models.py
@@ -2,13 +2,15 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.db import models
+from django.contrib.auth.models import User
 
 
 class Ticket(models.Model):
-    # Your Ticket model definition goes here
-    pass
-
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='images/', blank=True, null=True)
+    time_created = models.DateTimeField(auto_now_add=True)
 
 class Review(models.Model):
     ticket = models.ForeignKey(to=Ticket, on_delete=models.CASCADE)
@@ -39,3 +41,5 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
