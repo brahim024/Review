3831f202ac5bf3da838d31dcf02bdff9f6019cf7
joluhan <135083872+joluhan@users.noreply.github.com>
2024-01-04 11:01:16 +0100

    Summary

--- review/ltreview_app/views.py
+++ review/ltreview_app/views.py
@@ -6,6 +6,6 @@ from django.views import generic
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
-    template_name = 'templates/registration/signup.html'
+    template_name = 'registration/signup.html'
 
 
--- review/review/urls.py
+++ review/review/urls.py
@@ -19,13 +19,16 @@ from django.urls import path
 from ltreview_app.views import SignUpView
 from django.contrib.auth.views import LoginView, LogoutView
 
-urlpatterns = [
-    path('signup/', SignUpView.as_view(), name='signup'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
-]
+# urlpatterns = [
+#     path('signup/', SignUpView.as_view(), name='signup'),
+#     path('login/', LoginView.as_view(), name='login'),
+#     path('logout/', LogoutView.as_view(), name='logout'),
+# ]
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('signup/', SignUpView.as_view(), name='signup'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
