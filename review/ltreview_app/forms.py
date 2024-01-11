914dc18fb8cbb0b8fbf83ece9a22fbcaf23a0436
joluhan <135083872+joluhan@users.noreply.github.com>
2024-01-11 09:57:07 +0100

    Création de la Vue pour Ajout de Billet

--- review/ltreview_app/forms.py
+++ review/ltreview_app/forms.py
@@ -1,9 +1,15 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
+from .models import Ticket
+
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = get_user_model()
         fields = UserCreationForm.Meta.fields + ('email',)
 
+class TicketForm(forms.ModelForm):
+    class Meta:
+        model = Ticket
+        fields = ['title', 'description', 'image']
--- review/ltreview_app/views.py
+++ review/ltreview_app/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 # from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
+from django.shortcuts import render, redirect
 from .forms import CustomUserCreationForm
 from .models import Ticket
 from .models import Review
 from .models import UserFollows
 from django.shortcuts import get_object_or_404
+from .forms import TicketForm
 
 class SignUpView(generic.CreateView):
     form_class = CustomUserCreationForm
@@ -25,6 +27,14 @@ def user_follows_view(request):
     userfollows = UserFollows.objects.filter(user=request.user)
     return render(request, 'userfollows.html', {'userfollows': userfollows})
 
-
+def add_ticket(request):
+    if request.method == 'POST':
+        form = TicketForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = TicketForm()
+    return render(request, 'add_ticket.html', {'form': form})
 
 
